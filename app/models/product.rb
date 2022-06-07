ea15561be7947bb3bf6ec0e322fc4511fa97c1a4
Pedro Bueno <pbuenolima@gmail.com>
2022-06-07 16:01:50 -0300

    Index page almost done

--- app/models/product.rb
+++ app/models/product.rb
@@ -2,4 +2,11 @@ class Product < ApplicationRecord
   has_many :lines
   validates :name, :brand, :category, :price, :market, :address, presence: true
   validates :price, numericality: { greater_than: 0 }
+
+  include PgSearch::Model
+  pg_search_scope :search_by_name,
+                  against: %i[name],
+                  using: {
+                    tsearch: { prefix: true }
+                  }
 end
