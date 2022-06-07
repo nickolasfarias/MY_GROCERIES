e30e18e35d3d60a46eef3405ed50c75dd93b06ff
Nickolas Farias <nickolasfarias@gmail.com>
2022-06-07 18:17:15 -0300

    maps

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,4 +1,6 @@
 class Product < ApplicationRecord
+  geocoded_by :address
+  after_validation :geocode, if: :will_save_change_to_address?
   has_many :lines
   validates :name, :category, :price, :market, :address, presence: true
   validates :price, numericality: { greater_than: 0 }
